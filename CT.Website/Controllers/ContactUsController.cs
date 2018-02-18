27aa86614a94171458949aef27fb60450559e212
Ancuta Gicoveanu <agi@nemetos.net>
2018-02-18 20:18:43 +0200

    Contact Us form component

---
+++ CT.Website/Controllers/ContactUsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using CT.Website.Models;
+
+namespace CT.Website.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        [HttpGet]
+        public ActionResult SendEmail()
+        {
+            User currentUser = new User();
+            return PartialView(currentUser);
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(User currentUser)
+        {
+            return PartialView(currentUser);
+        }
+    }
+}
---
+++ CT.Website/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace CT.Website.Models
+{
+	public class User
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public string Company { get; set; }
+
+		public string Email { get; set; }
+
+		public string Message { get; set; }
+	}
+}
