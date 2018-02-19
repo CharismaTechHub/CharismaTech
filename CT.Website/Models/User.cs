b1bf1ec82bb40be4d986bf0f851cefd411b2ab52
Ancuta Gicoveanu <agi@nemetos.net>
2018-02-19 21:30:20 +0200

    Form - data annotations.

--- CT.Website/Models/User.cs
+++ CT.Website/Models/User.cs
@@ -1,14 +1,20 @@
-﻿namespace CT.Website.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CT.Website.Models
 {
 	public class User
 	{
-		public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please add your first name")]
+        public string FirstName { get; set; }
 
-		public string LastName { get; set; }
+        [Required(ErrorMessage = "Please add your last name")]
+        public string LastName { get; set; }
 
 		public string Company { get; set; }
 
-		public string Email { get; set; }
+        [Required(ErrorMessage = "Please add your email address")]
+        [EmailAddress(ErrorMessage = "Please add a valid email address")]
+        public string Email { get; set; }
 
 		public string Message { get; set; }
 	}
