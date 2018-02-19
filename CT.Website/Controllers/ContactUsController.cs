7793a235e1dda5aaa5f08d80b319063642f7ab21
Ancuta Gicoveanu <agi@nemetos.net>
2018-02-19 23:55:19 +0200

    Contact Form - backend implementation,

--- CT.Website/Controllers/ContactUsController.cs
+++ CT.Website/Controllers/ContactUsController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using CT.Website.Models;
+using CT.Website.Repositories;
 
 namespace CT.Website.Controllers
 {
@@ -10,13 +11,25 @@ namespace CT.Website.Controllers
         public PartialViewResult SendEmail()
         {
             User currentUser = new User();
+            ViewBag.Subtitle = "Get in touch and start unlocking the power of your data today.";
             return PartialView(currentUser);
         }
 
         [HttpPost]
         public PartialViewResult SendEmail(User currentUser)
         {
-            return PartialView(currentUser);
+            bool result = EmailSenderRepository.SendNotification(currentUser);
+
+            if (result)
+            {
+                ViewBag.Subtitle = "Thank you very much for contacting us. We will get back to you soon.";
+                return PartialView("/Views/ContactUs/FormResult.cshtml");
+            }
+            else
+            {
+                ViewBag.Subtitle = "An error occured on the server. We are sorry for the inconvenience, please try again.";
+                return PartialView("/Views/ContactUs/FormResult.cshtml");
+            }
         }
     }
 }
