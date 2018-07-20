e1c0ac5f2fac691103992ba3e0cb4b87c47fd938
Cristina Dragos <cristina.prodan87@gmail.com>
2018-07-20 18:16:52 +0300

    our team adjustments

--- CT.Website/Models/Hero.cs
+++ CT.Website/Models/Hero.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CT.Website.Models
+﻿namespace CT.Website.Models
 {
     public class Hero
     {
-
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
