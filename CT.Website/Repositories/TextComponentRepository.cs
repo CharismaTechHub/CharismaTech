bbbda6bd38f80f8faa5326acbe010dc4c2f783de
Cristina Dragos <cristina.prodan87@gmail.com>
2019-10-01 17:47:08 +0300

    style changes

--- CT.Website/Repositories/TextComponentRepository.cs
+++ CT.Website/Repositories/TextComponentRepository.cs
@@ -7,9 +7,8 @@ namespace CT.Website.Repositories
         public static TextComponent AboutUsComponent()
         {
             TextComponent aboutUsComponent = new TextComponent();
-            aboutUsComponent.Title = "Who Are We";
-            aboutUsComponent.Description = @"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                        Aenean fermentum neque sit amet dolor ornare, eget pellentesque justo sollicitudin. Morbi tincidunt ligula elementum metus sodales porta.</p>
+            aboutUsComponent.Title = "Who We Are";
+            aboutUsComponent.Description = @"<p>We are three passionate developers who decided to take on the IT world, offering quality software solutions to our clients.</p>
                                         <p></p>
                                         <p>
                                         Aenean vitae sodales lacus. Ut lectus quam, dapibus ac rhoncus ut, mollis at urna. Cras nec pretium est. Ut ultricies quis nisl in imperdiet. 
@@ -22,7 +21,7 @@ namespace CT.Website.Repositories
                                         Donec varius ac ex eget malesuada. Nunc rutrum nunc nec cursus fringilla. Praesent sollicitudin metus quam, a commodo neque ullamcorper vel.</p>";
             aboutUsComponent.LinkText = "Want to know more?";
             aboutUsComponent.LinkUrl = "/about-us";
-            aboutUsComponent.ColorClass = "section-color-light-white";
+            aboutUsComponent.ColorClass = "section-color-light-black";
 
             return aboutUsComponent;
         }
@@ -34,7 +33,7 @@ namespace CT.Website.Repositories
             ourTeamComponent.Description = @"<p>Our team is prepared to support you and your business decisions.</p>";
             ourTeamComponent.LinkText = "More About Us";
             ourTeamComponent.LinkUrl = "/our-team";
-            ourTeamComponent.ColorClass = "section-color-light-black";
+            ourTeamComponent.ColorClass = "section-color-light-white";
 
             return ourTeamComponent;
         }
@@ -46,7 +45,7 @@ namespace CT.Website.Repositories
             ourServicesComponent.Description = @"<p>Need help developing or improving your software service or application? Maybe we can help you!</p>";
             ourServicesComponent.LinkText = "Check out more";
             ourServicesComponent.LinkUrl = "/our--sevices";
-            ourServicesComponent.ColorClass = "section-color-light-white";
+            ourServicesComponent.ColorClass = "section-color-light-black";
 
             return ourServicesComponent;
         }
